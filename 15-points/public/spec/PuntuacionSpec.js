4cda3b2da90aa9f0070517012d7479dc81ccfa94
Carlos Biedma Tapia <carlos.biedma.tapia@gmail.com>
2013-10-28 17:50:24 +0100

    terminados los test de puntuacionspec, falta arreglar el collision spec

--- 15-points/public/spec/PuntuacionSpec.js
+++ 15-points/public/spec/PuntuacionSpec.js
@@ -19,6 +19,7 @@ describe("class PuntuacionSpec", function(){
 		SpriteSheet = {
                map : {enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
                 	explosion: { sx: 0, sy: 64, w: 64, h: 64, frames: 12 },
+                	fireball: { sx: 0, sy: 64, w: 64, h: 64, frames: 12 },
                 	missile: { sx: 0, sy: 64, w: 64, h: 64, frames: 1 }}
                 };
 		gameboard = new GameBoard();
@@ -64,6 +65,23 @@ describe("class PuntuacionSpec", function(){
 		loseGame();
 		expect(Game.points).toBe(0);
 	
+	
+	});
+	
+	it("bola de fuego con enemigo()", function(){
+		Game={width: 320, height:480};
+		var enem =new Enemy({ x: 100, y: -50, sprite: 'enemy_purple', B: 100, C: 2 , E: 100, health:10 });
+		var fireball = new FireBall(100,100, 5);
+		var puntos =new GamePoints();
+		enem.board= {add: function(obj) {}, remove: function () {return true}};
+		
+		gameboard.add(fireball);
+        gameboard.add(enem);
+        expect(Game.points).toBe(0);
+        gameboard.step(0.0000001);
+        enem.hit(10);
+        expect(Game.points).toBe(100);
+
 	
 	});
 
